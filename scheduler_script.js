83e0ea742040780e3067d3adcf5a9893a7e208f4
AchyutPal21 <achyut21pal@gmail.com>
2022-03-12 14:28:08 +0530

    add scheduler_script for removing expired files

---
+++ scheduler_script.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const connectDB = require("./config/db");
+connectDB();
+const File = require("./models/modelFile");
+
+async function deleteData() {
+  try {
+    const pastDate = new Date(Date.now() - 86400000);
+    const files = await File.find({ createdAt: { $lt: pastDate } });
+
+    let delFiles = 0;
+    if (files.length) {
+      console.log(`Expired files:${files.length}`);
+      for (let file of files) {
+        fs.unlinkSync(file.path);
+        await file.remove();
+        console.log(
+          `Successfully deleted ${
+            file.filename
+          }\nDeleted file Count:${++delFiles}`
+        );
+      }
+    } else {
+      console.log(`No Expired file found.`);
+    }
+
+    console.log("Expired files check successful !!!");
+  } catch (err) {
+    console.error("🔥 While deleting file", err);
+  }
+}
+
+deleteData().then((data) => {
+  console.log("Closing File Check...");
+  process.exit();
+});
