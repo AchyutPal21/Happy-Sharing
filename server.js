b43c9b4a7b087bec86a504a1673227307c2a9da7
AchyutPal21 <achyut21pal@gmail.com>
2022-03-12 12:52:10 +0530

    add cors

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 // Imports
 const connectDB = require("./config/db");
 connectDB();
+
+const cors = require("cors");
+
+// Cors
+const corsOptions = {
+  origin: process.env.ALLOWED_CORS_CLIENTS.split(","),
+};
+
 const app = require("./app");
+
+app.use(cors(corsOptions));
 
 // 👉 SERVER & PORT
 const port = process.env.PORT || 3000;
