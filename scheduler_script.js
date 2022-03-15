6465c3b6c435e52af61b98d612cc7c8b2c42b772
Achyut Pal <61113012+AchyutPal21@users.noreply.github.com>
2022-03-15 11:39:34 +0530

    Update scheduler_script.js
    updated path

--- scheduler_script.js
+++ scheduler_script.js
@@ -13,7 +13,7 @@ async function deleteData() {
     if (files.length) {
       console.log(`Expired files:${files.length}`);
       for (let file of files) {
-        fs.unlinkSync(`${file.path}`);
+        fs.unlinkSync(path.join(__dirname, `/${file.path}`));
         console.log(path.join(__dirname, `/${file.path}`));
         await file.remove();
         console.log(
